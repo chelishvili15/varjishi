dd150cd8383ec27298deb637731d67b1d9cd8797
dato <qutaiseli15@gmail.com>
2021-01-26 07:31:41 +0000

    add p tags

--- JavascriptDom.js
+++ JavascriptDom.js
@@ -1,11 +1,17 @@
-function jsStyle(){
-    text.style.fontSize = '24px';
-    text.style.color = 'green';
-    text.style.backgroundColor = 'red';
-}
-//------------------
+// function jsStyle(){
+//     text.style.fontSize = '24px';
+//     text.style.color = 'green';
+//     text.style.backgroundColor = 'red';
+// }
+// //------------------
 
-var myJSON = '{"name":"dato","city":"kutaisi"}';
-console.log(typeof(myJSON));
-var myObj = JSON.parse(myJSON);
-console.log(typeof(myObj));
+// var myJSON = '{"name":"dato","city":"kutaisi"}';
+// //console.log(typeof(myJSON));
+// var myObj = JSON.parse(myJSON);
+// //console.log((myObj));
+
+var p = document.getElementsByClassName('tag');
+
+//p[2].innerHTML = 'this is start'
+
+console.log(p);
